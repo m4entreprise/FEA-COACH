cf6e90c6e4dbf0382b94b436bed2d5ea9116b1e2
M4 Entreprise <thomas.m4entreprise@gmail.com>
2025-12-06 02:52:11 +0100

    Update LemonSqueezyService.php

--- app/Services/LemonSqueezyService.php
+++ app/Services/LemonSqueezyService.php
@@ -64,7 +64,7 @@ class LemonSqueezyService
                     'attributes' => [
                         'checkout_data' => $checkoutData,
                         'checkout_options' => [
-                            'embed' => false,
+                            'embed' => true,
                             'media' => true,
                             'logo' => true,
                             'desc' => true,
